4c1013e460a9b9d959a05c4a14e9e36e5e0874f2
rahmanhas <rahmanhas@gmail.com>
2023-04-30 01:47:37 +0600

    updated vercel file

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
-const categories = require('../the-news-dragon-server/data/categories.json')
+
+const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.use(cors());
 
@@ -13,6 +15,25 @@ app.get('/',(req,res)=>{
 app.get('/categories', (req,res)=>{
     res.send(categories)
 })
+app.get('/categories/:id', (req,res)=>{
+    const id = req.params.id;
+    if(id == 0){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n=>n.category_id === id);
+     res.send(categoryNews);
+    }
+})
+app.get('/news', (req,res)=>{
+    res.send(news)
+})
+app.get('/news/:id', (req,res)=>{
+    const id = req.params.id
+    const selectedNews = news.find(n=> n._id === id);
+    res.send(selectedNews);
+})
+
 
 app.listen(port, ()=>{
     console.log(`Dragon API is running on port: ${port}`);
